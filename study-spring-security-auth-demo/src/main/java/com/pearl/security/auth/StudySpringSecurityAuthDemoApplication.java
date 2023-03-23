30d1eb61b74a0f672e658c4e090a3a886a21f812
‘anonymous ’ <2578889892@qq.com>
2023-03-23 11:45:24 +0800

    11111111

---
+++ study-spring-security-auth-demo/src/main/java/com/pearl/security/auth/StudySpringSecurityAuthDemoApplication.java
@@ -0,0 +1,13 @@
+package com.pearl.security.auth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudySpringSecurityAuthDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StudySpringSecurityAuthDemoApplication.class, args);
+    }
+
+}
