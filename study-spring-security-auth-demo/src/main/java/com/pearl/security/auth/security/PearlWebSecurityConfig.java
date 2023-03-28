fc500eb1dced8db1224f39f9aa787214bcf0e45a
TD <2578889892@qq.com>
2023-03-28 22:37:03 +0800

    11111

--- study-spring-security-auth-demo/src/main/java/com/pearl/security/auth/security/PearlWebSecurityConfig.java
+++ study-spring-security-auth-demo/src/main/java/com/pearl/security/auth/security/PearlWebSecurityConfig.java
@@ -3,15 +3,39 @@ package com.pearl.security.auth.security;
 import com.pearl.security.auth.service.impl.UserDetailsServiceImpl;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AuthorizeHttpRequestsConfigurer;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
-// 开启 Spring Security，debug：是否开启Debug模式
 @EnableWebSecurity(debug = false)
 public class PearlWebSecurityConfig {
+
+    @Bean
+    SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
+        // 配置所有的Http请求必须认证
+        http.authorizeHttpRequests()
+                .requestMatchers("/login.html").permitAll()
+                .anyRequest().authenticated();
+        // 开启表单登录
+        http.formLogin()
+                .successForwardUrl("/index") // 自定义登录成功后转发的地址（请求转发，地址栏不变）
+                //.defaultSuccessUrl("/index")// 自定义登录成功后重定向的地址（重定向，地址栏变），会优先跳转到登陆前访问的页面，也可以设置总是跳转到该地址
+                .loginPage("/login.html") // 自定义登录页面（注意要同步配置loginProcessingUrl）
+                .loginProcessingUrl("/custom/login") // 自定义登录处理URL
+                .usernameParameter("name") // 自定义用户名参数名称
+                .passwordParameter("pwd");   //自定义密码参数名称
+        // 开启Basic认证
+        http.httpBasic();
+        // 关闭 CSRF
+        http.csrf().disable();
+        return http.build();
+    }
 
     /**
      * 密码器
