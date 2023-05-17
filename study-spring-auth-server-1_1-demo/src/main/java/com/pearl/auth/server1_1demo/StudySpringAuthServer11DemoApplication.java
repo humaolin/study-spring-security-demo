94945b4b931b15d8b14181edc037622d21f4c225
‘anonymous ’ <2578889892@qq.com>
2023-05-17 18:05:50 +0800

    111111111111

---
+++ study-spring-auth-server-1_1-demo/src/main/java/com/pearl/auth/server1_1demo/StudySpringAuthServer11DemoApplication.java
@@ -0,0 +1,13 @@
+package com.pearl.auth.server1_1demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudySpringAuthServer11DemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StudySpringAuthServer11DemoApplication.class, args);
+    }
+
+}
---
+++ study-spring-auth-server-1_1-demo/src/test/java/com/pearl/auth/server1_1demo/StudySpringAuthServer11DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pearl.auth.server1_1demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudySpringAuthServer11DemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
