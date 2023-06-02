cd0333de8d3b4fb50c3d2b3b674a923df66ce7c0
‘anonymous ’ <2578889892@qq.com>
2023-06-02 18:13:56 +0800

    11111

--- study-spring-security-authorize-demo/src/main/java/com/pearl/authorize/demo/dynamic/AuthRule.java
+++ study-spring-security-authorize-demo/src/main/java/com/pearl/authorize/demo/dynamic/AuthRule.java
@@ -1,49 +1,29 @@
 package com.pearl.authorize.demo.dynamic;
 
 import cn.hutool.core.collection.CollUtil;
+import lombok.AllArgsConstructor;
+import lombok.Data;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 
 /**
+ * 新建
  * @author TangDan
  * @version 1.0
  * @since 2023/6/1
  */
+@Data
+@AllArgsConstructor
 public class AuthRule {
 
+    // 请求路径
     String url;
 
-    List<GrantedAuthority> roles;
-
-    public static Map<String, AuthRule> init() {
-        return new HashMap<>() {{
-            put("/test001", new AuthRule("/test001", CollUtil.newArrayList(new SimpleGrantedAuthority("ROLE_ADMIN"))));
-            put("/test002", new AuthRule("/test002", CollUtil.newArrayList(new SimpleGrantedAuthority("ROLE_USER"))));
-        }};
-    }
-
-    public String getUrl() {
-        return url;
-    }
-
-    public void setUrl(String url) {
-        this.url = url;
-    }
-
-    public List<GrantedAuthority> getRoles() {
-        return roles;
-    }
-
-    public void setRoles(List<GrantedAuthority> roles) {
-        this.roles = roles;
-    }
-
-    public AuthRule(String url, List<GrantedAuthority> roles) {
-        this.url = url;
-        this.roles = roles;
-    }
+    // 请求路径对应的 角色+权限值
+    Set<String> authoritySet;
 }
--- study-spring-security-authorize-demo/src/main/java/com/pearl/authorize/demo/dynamic/CustomAuthorizationManager.java
+++ study-spring-security-authorize-demo/src/main/java/com/pearl/authorize/demo/dynamic/CustomAuthorizationManager.java
@@ -1,17 +1,15 @@
 package com.pearl.authorize.demo.dynamic;
 
-import cn.hutool.core.util.ObjectUtil;
-import com.pearl.authorize.demo.config.JdbcAuthorizationManager;
+import cn.hutool.core.collection.CollUtil;
+import lombok.RequiredArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.security.authorization.AuthorizationDecision;
 import org.springframework.security.authorization.AuthorizationManager;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.authority.AuthorityUtils;
 import org.springframework.security.web.access.intercept.RequestAuthorizationContext;
 
-import java.util.List;
 import java.util.Map;
 import java.util.Set;
 import java.util.function.Supplier;
@@ -21,28 +19,30 @@ import java.util.function.Supplier;
  * @version 1.0
  * @since 2023/6/1
  */
+@RequiredArgsConstructor
 public class CustomAuthorizationManager implements AuthorizationManager<RequestAuthorizationContext> {
 
-    private static final Logger log = LoggerFactory.getLogger(JdbcAuthorizationManager.class);
+    private static final Logger log = LoggerFactory.getLogger(CustomAuthorizationManager.class);
 
-    private static final Map<String, AuthRule> authRules = AuthRule.init();
+    private final UrlSecurityMetadataSource dbUrlSecurityMetadataSource;
 
     @Override
     public AuthorizationDecision check(Supplier<Authentication> authentication, RequestAuthorizationContext context) {
+        String requestURI = context.getRequest().getRequestURI(); // 请求路径
         // 1. 可以添加动态添放行路径，然后判断是否放行，若是，则返回 true
+        if (requestURI.equals("/login")) { // 登录放行
+            return new AuthorizationDecision(true);
+        }
         // .... 自己实现
         // 2. 所有请求必须都经过认证
         if (!authentication.get().isAuthenticated()) {
             return new AuthorizationDecision(false);
         }
         // 3. 根据请求路径查询权限
-        String requestURI = context.getRequest().getRequestURI(); // 请求路径
-        AuthRule authRule = authRules.get(requestURI); // 模拟数据库或缓存中查询该路径对应的规则
-        if (ObjectUtil.isNotNull(authRule)) {
+        Set<String> attributes = dbUrlSecurityMetadataSource.getAttributes(requestURI);// 模拟数据库或缓存中查询该路径对应的规则
+        if (CollUtil.isNotEmpty(attributes)) {
             // 存在规则，进行校验
-            List<GrantedAuthority> roles = authRule.getRoles(); // 该规则需要的所有权限值
-            Set<String> authorities = AuthorityUtils.authorityListToSet(roles);
-            return new AuthorizationDecision(isAuthorized(authorities, authentication.get()));
+            return new AuthorizationDecision(isAuthorized(attributes, authentication.get()));
         }
         // 原来逻辑
         // 1. 启动加载配置中的所有配置，设置为AuthorizationManager 到过滤器中
@@ -66,4 +66,6 @@ public class CustomAuthorizationManager implements AuthorizationManager<RequestA
         }
         return false;
     }
+
+
 }
